2820afbd5faafab074b5e65ea8f18f2bfd0a04aa
SatyaSri96 <pillasaiveni0345@gmail.com>
2026-01-22 15:08:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let result = document.getElementById("result");x
+function appendValue(value) {
+    result.value += value;
+}
+
+function clearResult() {
+    result.value = "";
+}
+
+function deleteLast() {
+    result.value = result.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        result.value = eval(result.value);
+    } catch {
+        result.value = "Error";
+    }
+}
